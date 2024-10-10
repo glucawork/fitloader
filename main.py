03dfe4e4d94307247d1b8023da874333f8242cb6
gianluca <gianluca.rossi@uniroma2.eu>
2024-10-10 17:00:00 +0200

    fixed timestamp problem + add icon

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from qgis.PyQt.QtWidgets import QAction, QFileDialog, QMessageBox
 from qgis.core import QgsProject, QgsFeature, QgsGeometry, QgsPointXY, QgsVectorLayer, QgsField, QgsMessageLog, QgsFields
 from PyQt5.QtCore import QVariant, QMetaType
+from PyQt5.QtGui import QIcon
 from fitparse import FitFile
+import os
 
 class FitLoaderPlugin:
     def __init__(self, iface):
@@ -11,8 +13,11 @@ class FitLoaderPlugin:
 
     def initGui(self):
         """Create and add the plugin's action to the QGIS GUI."""
+        
+        icon_path = os.path.join(os.path.dirname(__file__), "icons", "fit_loader.png")
+        
         # Create an action in the main QGIS window
-        self.action = QAction("Load FIT Activity", self.iface.mainWindow())
+        self.action = QAction(QIcon(icon_path), "Load FIT Activity", self.iface.mainWindow())
         # Connect the action to the run method
         self.action.triggered.connect(self.run)
         # Add the action to the QGIS plugin menu under "FitLoader"
@@ -31,14 +36,17 @@ class FitLoaderPlugin:
         if not fit_path:
             return  # If no file was selected, exit the function
 
-        try:
+        fitfile = FitFile(fit_path)
+        self.create_layer_from_fit(fitfile)
+            
+        #try:
             # Load the selected FIT file using the fitparse library
-            fitfile = FitFile(fit_path)
+        #    fitfile = FitFile(fit_path)
             # Call the function to create a layer from the FIT file data
-            self.create_layer_from_fit(fitfile)
-        except Exception as e:
+        #    self.create_layer_from_fit(fitfile)
+        #except Exception as e:
             # Display an error message if loading the file fails
-            QMessageBox.critical(None, "Error", f"Failed to load FIT file: {e}")
+        #    QMessageBox.critical(None, "Error", f"Failed to load FIT file: {e}")
 
     def create_layer_from_fit(self, fitfile):
         """Create a point layer in QGIS based on the records from the FIT file."""
@@ -69,15 +77,15 @@ class FitLoaderPlugin:
             
             field_set = field_set.union(set(dict_record.keys()))
             
-        field_list = list(field_set)
+        fields_list = list(field_set)
         
         fields =  QgsFields()
 
-        for field in field_list:
+        for field in fields_list:
             if field in ('timestamp', 'activity_type'):
                 fields.append( QgsField(field, QMetaType.QString) )
             else:
-                fields.append( QgsField(field, QMetaType.Double) )
+                fields.append( QgsField(field, QMetaType.QString) )
         
         pr.addAttributes(fields)
         layer.updateFields()
@@ -97,22 +105,27 @@ class FitLoaderPlugin:
             dict_record['position_lat'] = dict_record['position_lat']*180/2**31
             dict_record['position_long'] = dict_record['position_long']*180/2**31
             
-            QgsMessageLog.logMessage(str(dict_record['position_lat']), 'FitLoader', 0)
+            #QgsMessageLog.logMessage(str(dict_record['position_lat']), 'FitLoader', 0)
 
             # Create a point geometry with longitude and latitude
             point = QgsPointXY(dict_record['position_long'], dict_record['position_lat'])
             # Create a new feature for the point
-            feature = QgsFeature(fields)
-            feature.setGeometry(QgsGeometry.fromPointXY(point))
+            feature = QgsFeature()
+            feature.setFields(fields)
+            
             # Set the feature's attributes (timestamp, lat, lon, speed, and cadence)
             for x in dict_record:
-                feature.setAttribute(x, dict_record[x] )
+                #QgsMessageLog.logMessage(x  , 'FitLoader', 0)
+                if x in ('timestamp', 'activity_type'):
+                    feature.setAttribute(x, str(dict_record[x]) )
+                else:
+                    feature.setAttribute(x, dict_record[x] )
             # Add the feature to the list of features
-            features.append(feature)
+            feature.setGeometry(QgsGeometry.fromPointXY(point))
+            pr.addFeature(feature)
 
 
         # Add all the features to the layer
-        pr.addFeatures(features)
-        layer.updateFields()
+        #pr.addFeatures(features)
         # Add the layer to the current QGIS project
         QgsProject.instance().addMapLayer(layer)
